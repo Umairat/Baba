79037603558f35037cf6752fa1d0cd62d3037ab7
Sanuka11111111121 <kaveenmd433@gmail.com>
2024-12-01 20:51:28 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=bMtTCIzI#fQFlE8l4_kiWFGQQXjKdMcTDVy-bip9s_THiJI1sbNM",
 };
